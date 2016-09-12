b76a2d589d2a468c8af9a4e53fbc375df82cc91c
jc247417 <justin.peever@my.jcu.edu.au>
2016-09-12 10:26:01 +1000

    Completeing task 1

---
+++ usedCars.py
@@ -0,0 +1,32 @@
+"""
+CP1404/CP5632 Practical
+Client code to use the Car class
+Note that the import has a folder (module) in it.
+"""
+from car import Car
+
+
+def main():
+
+    limo = Car(100)
+    limo.name = "limo"
+    Car.add_fuel(limo, 20)
+    limo.drive(115)
+    print("fuel =", limo.fuel)
+    print("odo =", limo.odometer)
+    print(limo)
+    limo._str_()
+    print("Car {}, {}".format(limo.fuel, limo.odometer))
+    print("Car {self.fuel}, {self.odometer}".format(self=limo))
+
+
+    bus = Car(180)
+    bus.drive(30)
+    print("fuel =", bus.fuel)
+    print("odo =", bus.odometer)
+    print(bus)
+
+    print("Car {}, {}".format(bus.fuel, bus.odometer))
+    print("Car {self.fuel}, {self.odometer}".format(self=bus))
+
+main()
